21f1fe3011b2a7f291036c872923be5fefea4de3
Leonid Sviderskii <svidersky.leonid@gmail.com>
2017-01-28 16:21:21 -0500

    Inverted register is done

---
+++ programming-basics/InvertedRegister.js
@@ -0,0 +1,15 @@
+/*
+The function changes a register of every symbols in a string.
+*/
+
+export const invertCase = (str) => {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      result += str[i].toLowerCase();
+    } else {
+      result += str[i].toUpperCase();
+    }
+  }
+  return result;
+};
